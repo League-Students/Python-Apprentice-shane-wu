f150e2a10ac5e9645baccdf72410985a87ab8d73
League Student <student@jointheleague.org>
2026-07-02 12:40:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
+++ lessons/10_Turtles/30_Turtle_Tricks/10_Turtle_Tricks.py
@@ -17,7 +17,7 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
 # tina.pencolor()
-
+tina.forward
 ... # Your code here
 
 turtle.exitonclick()                    # Close the window when we click on it
