fe373635b2b879f69c0d7b6608a8a09d81338c68
League Student <student@jointheleague.org>
2026-07-07 13:07:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -25,19 +25,19 @@ window.setup(width=600, height=600, startx=0, starty=0)
 my_turtle = turtle.Turtle()
 my_turtle.shape("turtle")
 my_turtle.speed(0)
-my_turtle.width(100)
+my_turtle.width(10)
 
 sides = 1000
 angle = 360 / sides
 
-for i in range(1000000):
-    if i == 10000:
-        my_turtle.width(50000)
-    if i == 15000:
-        my_turtle.width(1000)
+for i in range(100):
+    if i == 100:
+        my_turtle.width(500)
+    if i == 150:
+        my_turtle.width(100)
     my_turtle.pencolor(get_next_color(i))
     my_turtle.forward(i)
-    my_turtle.right(angle + 100000)
+    my_turtle.right(angle + 100)
 
 my_turtle.hideturtle()
 
