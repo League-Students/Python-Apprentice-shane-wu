4b3885372f727322428371c847fd6e697acb2643
League Student <student@jointheleague.org>
2026-07-07 12:56:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -14,22 +14,5 @@ import turtle
 t = turtle.Turtle()
 turtle.setup(600, 600, 0, 0)
 window = turtle.screen()
-
-# 1) Complete make_a_shape() to make the turtle move in some pattern. 
-# For instance, you can make it go left 30 degrees, then forward 50 pixels, 
-# then right 60 degrees, then forward 100 pixels. Make any shape you like.
-
-def make_a_shape(t):
-    """Make a shape with turtle t. Make it go left or right or forward"""    
-    ...
-
-# 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
-# For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
-# The second ... in the for loop should be the number of shapes you want to make,
-# for example 100, or a list of numbers.
-
-num_shapes = ...
-
-for i in range(...):
-    make_a_shape(t)
-    t.right(360/num_shapes)
+left = 90
+forward(2)
