23dce4375bf2e79f965413133e14bc80ceaf1fdf
League Student <student@jointheleague.org>
2026-07-08 12:50:25 -0700

    Automated commit

--- lessons/10_Turtles/90_Graphics_Projects/10_Flaming_Ninja_Star.py
+++ lessons/10_Turtles/90_Graphics_Projects/10_Flaming_Ninja_Star.py
@@ -12,7 +12,7 @@ def fractal_triangle(size,depth):
     else:
         for i in range(3):
             fractal_triangle(size/2,depth-1)
-            tina.forward(size/2)
+            tina.forward(size)
             tina.left(120)
 
 fractal_triangle(200,1)
