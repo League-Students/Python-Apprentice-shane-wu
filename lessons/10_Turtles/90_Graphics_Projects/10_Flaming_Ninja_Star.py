db145a366ae020f9336bed82c8d5839791cfd357
League Student <student@jointheleague.org>
2026-07-08 12:38:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/10_Flaming_Ninja_Star.py
+++ lessons/10_Turtles/90_Graphics_Projects/10_Flaming_Ninja_Star.py
@@ -5,7 +5,12 @@ screen = turtle.Screen()
 screen.setup(600,600)
 
 def fractal_triangle(size,depth)
-    
+    if depth == 0:
+        for i in range(3):
+            tina.forward(size)
+            tina.left(120)
+
+fractal
 
 
 turtle.exitonclick()
