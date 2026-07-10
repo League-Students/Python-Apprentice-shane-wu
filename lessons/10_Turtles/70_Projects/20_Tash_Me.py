59ef8a9304198b81b71fb200093a2c4256ab20af
League Student <student@jointheleague.org>
2026-07-10 12:40:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/70_Projects/20_Tash_Me.py
+++ lessons/10_Turtles/70_Projects/20_Tash_Me.py
@@ -7,10 +7,11 @@ screen.setup(500,500)
 cam_colors = ["red", "black", "white", "blue", "green"]
 
 def open_cam_1():
+    print("cam 1 open")
 
 
 screen.listen()
-screen.onkey(open_cam_1, "1")
+screen.onkey(open_cam_1 , "1")
 
 
 
