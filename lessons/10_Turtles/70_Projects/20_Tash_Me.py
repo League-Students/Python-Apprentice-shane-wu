c85a0ade646e3973326d2c1773a4d76ddb1c43e5
League Student <student@jointheleague.org>
2026-07-10 12:42:22 -0700

    Automated commit

--- lessons/10_Turtles/70_Projects/20_Tash_Me.py
+++ lessons/10_Turtles/70_Projects/20_Tash_Me.py
@@ -5,7 +5,7 @@ tina = turtle.Turtle()
 screen = turtle.Screen()
 screen.setup(500,500)
 
-cam_colors = ["white", "blue", "white", "blue", "green"]
+cam_colors = ["white", "blue", "red", "black", "green"]
 
 def open_cam_1():
     print("cam 1 open")
